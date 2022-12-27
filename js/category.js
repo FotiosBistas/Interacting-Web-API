273aeec637fa6c2d1a02a8ecf19bb6ed4be10d68
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-27 23:57:22 +0200

    adding filter to radio

--- js/category.js
+++ js/category.js
@@ -16,7 +16,7 @@ let url_subcategories_products = new URL(`https://wiki-shop.onrender.com/categor
 fetch(url_subcategories_products, {
     method: 'GET',
 })
-.then( (responseText) =>{
+.then((responseText) =>{
     if (responseText.status >= 200 && responseText.status < 300){
         return responseText.json(); 
     }else{
@@ -63,7 +63,31 @@ function createRadio(options){
     outputHTML.innerHTML = ourHTML;
 }
 
+let radio_container = document.getElementById('products-aside');
 
+radio_container.onclick = function(event){
+    if(event.target.type == "radio"){
+        let children = document.getElementById("show_products").children;
+        let data = []; 
+
+        for (let i = 0; i < children.length; i++) {
+            let nobject = {}; 
+            nobject.id = children[i].id; 
+            nobject.subcategory_id = children[i].dataset.subcategory; 
+            nobject.title = children[i].getElementById(); 
+            nobject.cost = children[i].cost; 
+            nobject.description = children[i].description; 
+            data.push(nobject);
+        }
+        products.innerHTML = ""; 
+
+        let rawTemplate = document.getElementById("products").innerHTML;
+        let compiledTemplate = Handlebars.compile(rawTemplate);
+        let ourHTML = compiledTemplate(data);
+        let outputHTML = document.getElementById("show_products");
+        outputHTML.innerHTML = ourHTML;
+    }
+}
 
 Handlebars.registerHelper('makeRadio', function(name, options){
     let html = '';
@@ -71,7 +95,7 @@ Handlebars.registerHelper('makeRadio', function(name, options){
         let option = options[i];
         html += `
         <label for="${option.id}">${option.title}</label>
-        <input type="radio" name="${name}" id="${option.id}">`
+        <input type="radio" data-category="${option.category_id}" name="${name}" id="${option.id}">`
     }
     return new Handlebars.SafeString(html);
 });
