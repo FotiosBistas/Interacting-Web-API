a4969e669b148fc8a351c59feab8113f28ac3faf
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-29 13:39:32 +0200

    setup express

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8080
+
+let ___dirname = "../public"
+
+
+
+function log(text){
+    var time = new Date();
+    console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
+
+app.listen(port, () => {
+    log(`App listening on port ${port}`)
+});
+
+app.use(express.static('public'));
+
+// parse url-encoded content from body
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json content from body
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    
+    var options = {
+        root: path.join(__dirname, 'public')
+    }
+
+    res.sendFile('index.html', options, function(err){
+        log(err)
+    })
+})
